d3ace9ebbde8a65d4be2151b0101f1a395ff4f55
John Clarence A Dela Cruz <jorenadelacruz@gmail.com>
2026-03-26 14:21:36 +0800

    made it into a tailwind not bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
+  server:{
 
-    server:{
+      host: true,
+      port: 4321,
+  },
 
-        host: true,
-        port: 4321,
-    }
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
